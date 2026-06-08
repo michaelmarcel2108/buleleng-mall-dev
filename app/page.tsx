d3e92c942ace20377cc130e1f1a1e8296b745807
michaelmarcel2108 <michaelmhartono@gmail.com>
2026-06-08 14:43:55 +0800

    fix article to post error

--- app/page.tsx
+++ app/page.tsx
@@ -95,7 +95,7 @@ export default async function Home() {
 
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 md:gap-6">
               {articles.map((article) => (
-                <ArticleCard key={article.id} article={article} />
+                <ArticleCard key={article.id} post={article} />
               ))}
             </div>
           </div>
